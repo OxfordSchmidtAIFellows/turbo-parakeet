ffa73bb1a0485d2e050829b1f7793f405dcf7b1c
elnazazizi <elnaz.azizi@eng.ox.ac.uk>
2023-06-02 12:28:48 +0100

    Fixed testing

--- pytimeops/tests/test_time_series.py
+++ pytimeops/tests/test_time_series.py
@@ -9,12 +9,13 @@ class TestTimeSeries(unittest.TestCase):
     def test_init(self):
         t = [1, 2, 3]
         v = [10, 100, 105]
-        md = ["s1","fruc","0mM","GRN1"]
+        md = ["s1", "fruc", "0mM", "GRN1"]
         ts = pto.TimeSeries(t, v, md)
 
         self.assertEqual(t, ts.time_index)
         self.assertEqual(v, ts.values)
         self.assertEqual(md, ts.metadata)
+
 
 if __name__ == '__main__':
     unittest.main()
