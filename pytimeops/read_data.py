1dfde6db50b364a083ee0068c9ca71838c9caa93
elnazazizi <elnaz.azizi@eng.ox.ac.uk>
2023-06-02 11:44:32 +0100

    Read the data

---
+++ pytimeops/read_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import numpy as np
+import pytimeops 
+
+t = range(10)
+def read_file(filename):
+
+    df = pd.read_csv(filename)
+    list_y = []
+    for i , row in df.iterrows():
+        
+        V = df.iloc[i][5:].values
+        MD = df.iloc[i][1:5].values
+        list_y.append(pytimeops.Timeseries(t,V,MD))
+    
+    return list_y
+
+
+list_1 = read_file('Fig1H-K_SI1G-I.csv')
+
+print([ts.metadata for ts in list_1])
--- pytimeops/time_series.py
+++ pytimeops/time_series.py
@@ -1,5 +1,22 @@
+class  Timeseries:
+    def __init__(self,t,v,md):
+        """
+        Parameters
+        ----------
 
-class TimeSeries:
-    def __init__(self, t, x):
-        self.t = t
-        self.x = x
+        time_index (t): list of int
+            time samples (in this case bins)
+
+        values (v): list of int
+            Values of each sample (in this case [GRN1,GRN2,GRN3])
+
+        metadata (md): dictionary
+            Other attributes of the dataset which can be numerical or non-numerical
+            (in this case: sensillum (non-numerical), sugar(non-numerical), Concentration (numerical))   
+
+        """
+        self.time_index = t
+        self.values = v
+        self.metadata = md
+
+
