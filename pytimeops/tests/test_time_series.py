8cbcb6718412e36f2969e5af8829b787dfe6a5e1
elnazazizi <elnaz.azizi@eng.ox.ac.uk>
2023-06-02 12:24:33 +0100

    Fix testing code

--- pytimeops/tests/test_time_series.py
+++ pytimeops/tests/test_time_series.py
@@ -8,12 +8,13 @@ class TestTimeSeries(unittest.TestCase):
 
     def test_init(self):
         t = [1, 2, 3]
-        y = [10, 100, 105]
-        ts = pto.TimeSeries(t, y)
+        v = [10, 100, 105]
+        md = ["s1","fruc","0mM","GRN1"]
+        ts = pto.TimeSeries(t, v, md)
 
-        self.assertEqual(t, ts.t)
-        self.assertEqual(y, ts.x)
-
+        self.assertEqual(t, ts.time_index)
+        self.assertEqual(v, ts.values)
+        self.assertEqual(md, ts.metadata)
 
 if __name__ == '__main__':
     unittest.main()
