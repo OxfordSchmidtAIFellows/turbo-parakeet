50c008f1bd3927a9066c37bbf502f68f0b249ac5
elnazazizi <elnaz.azizi@eng.ox.ac.uk>
2023-06-04 10:17:19 +0100

    Fixed style

--- pytimeops/read_data.py
+++ pytimeops/read_data.py
@@ -1,20 +1,18 @@
 import pandas as pd
 import pytimeops
 
-t = range(10)
-
-
 def read_file(filename):
 
     df = pd.read_csv(filename)
+    t = range(10)
     list_y = []
     for i, row in df.iterrows():
-        V = df.iloc[i][5:].values
-        MD = df.iloc[i][1:5].values
+        V = row[5:].values
+        MD = dict(row[1:5])
         list_y.append(pytimeops.Timeseries(t, V, MD))
     return list_y
 
 
-list_1 = read_file('Fig1H-K_SI1G-I.csv')
+list_1 = read_file('Data/Fig1H-K_SI1G-I.csv')
 
 print([ts.metadata for ts in list_1])
