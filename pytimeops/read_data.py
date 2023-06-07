5c2cc85553e595373f6f85cd57814fd8c87bec1b
elnazazizi <elnaz.azizi@eng.ox.ac.uk>
2023-06-07 13:18:27 +0100

    Unit testing added

--- pytimeops/read_data.py
+++ pytimeops/read_data.py
@@ -2,18 +2,20 @@ import pandas as pd
 import pytimeops
 
 
-def read_file(filename):
+def read_file(filename, Num_md, Num_b):
 
     df = pd.read_csv(filename)
-    t = range(10)
+    t = range(Num_b)
     list_y = []
     for i, row in df.iterrows():
-        V = row[5:].values
-        MD = dict(row[1:5])
+        V = row[Num_md+1:].values
+        MD = dict(row[1:Num_md+1])
         list_y.append(pytimeops.Timeseries(t, V, MD))
     return list_y
 
 
-list_1 = read_file('Data/Fig1H-K_SI1G-I.csv')
+list_1 = read_file('Data/Fig1H-K_SI1G-I.csv', 4, 10)
 
-print([ts.metadata for ts in list_1])
+'#print([ts.metadata for ts in list_1])'
+'#print([ts.values for ts in list_1])'
+'#print(hasattr(pytimeops, read_file))'
---
+++ pytimeops/tests/test_read_data_2.py
@@ -0,0 +1,24 @@
+import unittest
+import pytimeops as pto
+
+
+class TestReadData(unittest.TestCase):
+    def test_read_file(self):
+        '# Test data'
+        test_data = 'Data/test_data.csv'
+        num_md = 4
+        num_b = 10
+        '# Call the function being tested'
+        '#result = pto.read_file(test_data, num_md, num_b)'
+        result = pto.read_file(test_data, 4, 10)
+        '# Assert the expected results'
+        self.assertIsInstance(result, list)
+        '# Check individual Timeseries objects'
+        for timeseries in result:
+            self.assertIsInstance(timeseries, pto.Timeseries)
+            self.assertEqual(len(timeseries.values), num_b)
+            self.assertEqual(len(timeseries.metadata), num_md)
+
+
+if __name__ == '__main__':
+    unittest.main()
